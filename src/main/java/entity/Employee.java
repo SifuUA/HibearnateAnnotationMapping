39a10a6f5cae6741f3f4abd6b0b1887568daf444
SifuUA <alekseykres@gmail.com>
2017-06-30 18:15:35 +0300

    add entity Emplooyee

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,70 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * Created by Alex on 30.06.2017.
+ */
+
+
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private Address address;
+
+    public Employee() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                ", address=" + address +
+                '}';
+    }
+}
